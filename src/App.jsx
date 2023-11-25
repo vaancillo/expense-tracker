c299bd60e38899f8f799460cb804f868785ccf6f
vancillo <yaredbane@gmail.com>
2023-11-24 21:50:14 -0600

    Add Balance and TransactionForm components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { GlobalProvider } from './context/GlobalState'
 import Header from './components/Header'
+import Balance from './components/Balance'
+import TransactionForm from './components/TransactionForm'
 
 function App () {
   return (
     <GlobalProvider>
       <Header />
+      <Balance />
+      <TransactionForm />
       <h1>React Context</h1>
     </GlobalProvider>
   )
--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,6 +1,15 @@
+import { useGlobalState } from '../context/GlobalState'
+
 function Balance () {
+  const data = useGlobalState()
   return (
-    <div>Balance</div>
+    <>
+      <div>Balance</div>
+      <div>
+        <h4>Your Balance</h4>
+        {JSON.stringify(data)}
+      </div>
+    </>
   )
 }
 export default Balance
