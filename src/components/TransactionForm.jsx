52010dc1925cf78f09dc14b2da538774cfb61921
vancillo <yaredbane@gmail.com>
2023-11-28 12:18:11 -0600

    Add transaction form and update global state

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,7 +1,36 @@
-function TransactionForm
-() {
+import { useState } from 'react'
+import { useGlobalState } from '../context/GlobalState'
+
+function TransactionForm () {
+  const { addTransaction } = useGlobalState()
+  const [description, setDescription] = useState()
+  const [amount, setAmount] = useState(0)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    addTransaction({
+      id: 1,
+      description,
+      amount
+    })
+  }
+
   return (
-    <div>TransactionForm</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text' placeholder='Enter a description'
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type='number' step='0.01' placeholder='Enter an amount'
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button>
+          Add a Transaction
+        </button>
+      </form>
+    </div>
   )
 }
 export default TransactionForm
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -14,8 +14,16 @@ export const useGlobalState = () => {
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
+
   return (
-    <Context.Provider value={{ transactions: state.transactions }}>
+    <Context.Provider value={{ transactions: state.transactions, addTransaction }}>
       {children}
     </Context.Provider>
   )
