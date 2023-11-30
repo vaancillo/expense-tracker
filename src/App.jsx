301553af23e76b24f460cd21b8bde2a092f38a49
vancillo <yaredbane@gmail.com>
2023-11-30 17:15:11 -0600

    Add IncomeExpenses component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from './components/Header'
 import Balance from './components/Balance'
 import TransactionForm from './components/transactions/TransactionForm'
 import TransactionList from './components/transactions/TransactionList'
+import IncomeExpenses from './components/IncomeExpenses'
 
 function App () {
   return (
     <GlobalProvider>
       <Header />
+      <IncomeExpenses />
       <Balance />
       <TransactionForm />
       <TransactionList />
---
+++ src/components/IncomeExpenses.jsx
@@ -0,0 +1,26 @@
+import { useGlobalState } from "../context/GlobalState"
+
+const IncomeExpenses = () => {
+
+    const { transactions } = useGlobalState()
+
+    const amount = transactions.map(transaction => transaction.amount)
+
+    const income = amount.filter(item => item > 0).reduce((acc, item) => (acc += item), 0)
+
+    const expense = amount.filter(item => item < 0).reduce((acc, item) => (acc += item), 0)
+
+  return (
+    <>
+      <div>
+        <h4>Income</h4>
+        <p>{income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p>{expense}</p>
+      </div>
+    </>
+  )
+}
+export default IncomeExpenses
