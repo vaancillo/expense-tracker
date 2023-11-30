98c91bb757cfbc5135702dd29cc1fae4254e7235
vancillo <yaredbane@gmail.com>
2023-11-29 20:06:47 -0600

    delete transaction function add

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -22,8 +22,15 @@ export const GlobalProvider = ({ children }) => {
     })
   }
 
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    })
+  }
+
   return (
-    <Context.Provider value={{ transactions: state.transactions, addTransaction }}>
+    <Context.Provider value={{ transactions: state.transactions, addTransaction, deleteTransaction }}>
       {children}
     </Context.Provider>
   )
